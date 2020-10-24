443a3fe4f6a86334b3e4912acde2a5cae68a92f9
piotr <299280@pw.edu.pl>
2020-10-25 01:51:11 +0200

    app.py tweaks

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
     
     return render_template('index.html')
